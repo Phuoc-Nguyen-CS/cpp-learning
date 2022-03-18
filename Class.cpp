5d7b8eb65183c418fe78396224f45faaa7e598f2
Phuoc Nguyen <phuocnguyen102800@gmail.com>
2022-03-18 14:29:17 -0700

    Created MyClass

--- Class.cpp
+++ Class.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+Class MyClass {
+    public:
+    int myNum;
+    string myString;
+};
+
+int main() {
+    MyClass myObj; // Creates an object of MyClass
+    
+    myObj.myNum = 0;
+    myObj.myNum = "Hello World";
+    
+    std::cout << myObj.myNum << std::endl << myObj.myString << std::endl;
+    
+    return 0;
+}
